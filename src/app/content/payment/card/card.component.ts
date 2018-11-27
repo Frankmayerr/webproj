66eb5bab03635b087eb34af0b84972f09100df45
u.gladkikh <u.gladkikh@skbkontur.ru>
2018-11-27 13:20:46 +0500

    one else

--- src/app/content/payment/card/card.component.ts
+++ src/app/content/payment/card/card.component.ts
@@ -9,20 +9,36 @@ import { AlertPromise } from 'selenium-webdriver';
 export class CardComponent implements OnInit {
   card: number;
   cvvNumber: string;
-  years = [];
   summary: number;
   comment: string;
-  valid : boolean;
+  email: string;
+  props: Map<string, boolean>;
 
   constructor() { 
-    for (var i = 2018; i <= 2035; i++) {
-      this.years.push({year: i});
-    }
+   this.props = new Map([['cardNumber', false], ['cvv', false], ['summary', false], ['comment', false], ['email', false]]);
+   alert(this.props);
+  }
+
+  get_confirm_actual()
+  {
+    var valid = true;
+    for (var key in this.props)
+      if (!this.props[key])
+        {
+          alert(key)
+          valid = false;
+          break;
+        }
+    if (valid)
+      $('#confirm-purchase').addClass('disabled')
+    else if ($('#confirm-purchase').hasClass('disabled'))
+      $('#confirm-purchase').removeClass('disabled')
   }
 
   inputValidation(objName, filterString) {
     var obj = $('#' + objName);
     var objFormClass = obj.closest('.form-group');
+
     obj.keypress(function(e) {
       e = e || event;
       var chr = '';
@@ -30,41 +46,40 @@ export class CardComponent implements OnInit {
       else chr = getChar(e);
       if (chr == null) 
         chr='';
-      var result = check(obj, filterString, chr);
-      if (this.valid && !result)
-        this.valid = false;
-      // return check(obj, filterString, chr);
+      var res = check(obj, filterString, chr);
+      alert(this);
+      this.props.set(objName, res);
+      this.get_confirm_actual();
   });
-  obj.keyup(function(e) {return check(obj, filterString, '');});
-  obj.click(function(e) {return check(obj, filterString, '');});
+    obj.keyup(function(e) {var res = check(obj, filterString, ''); this.props.set(objName, res); this.get_confirm_actual();});
+    obj.click(function(e) {var res = check(obj, filterString, ''); this.props.set(objName, res); this.get_confirm_actual();});
 
-  function getChar(event) {
-      if (event.which == null) {
-          if (event.keyCode < 32) return null;
-          return String.fromCharCode(event.keyCode) // IE
-      }
-      if (event.which != 0 && event.charCode != 0) {
-          if (event.which < 32) return null;
-          return String.fromCharCode(event.which) // остальные
-      }
-      return null; // специальная клавиша
-  }
-
-  function check(obj, filterString, chr){
-    if (filterString(obj.val() + chr)) {
-      obj.removeClass('alert-warning');
-      obj.addClass('alert-success');
-      objFormClass.removeClass('has-error');
-      objFormClass.addClass('has-success');
-      return 't';
+    function getChar(event) {
+        if (event.which == null) {
+            if (event.keyCode < 32) return null;
+            return String.fromCharCode(event.keyCode) // IE
+        }
+        if (event.which != 0 && event.charCode != 0) {
+            if (event.which < 32) return null;
+            return String.fromCharCode(event.which) // остальные
+        }
+        return null; // специальная клавиша
     }
-    obj.removeClass('alert-success');
-    obj.addClass('alert-warning');
-    objFormClass.removeClass('has-success');
-    objFormClass.addClass('has-error');
-    return 'f';
-  }
-  return 'f';
+
+    function check(obj, filterString, chr){
+      if (filterString(obj.val() + chr)) {
+        obj.removeClass('alert-warning');
+        obj.addClass('alert-success');
+        objFormClass.removeClass('has-error');
+        objFormClass.addClass('has-success');
+        return true;
+      }
+      obj.removeClass('alert-success');
+      obj.addClass('alert-warning');
+      objFormClass.removeClass('has-success');
+      objFormClass.addClass('has-error');
+      return false;
+    }
   }
 
   check_chars(str, filter) {
@@ -75,30 +90,19 @@ export class CardComponent implements OnInit {
   }
 
   validate_all() {
-    var isValidCard = this.inputValidation('cardNumber', (str) => (str.length == 16) && this.check_chars(str, (chr) => (chr >= '0' && chr <= '9')));
-    var isValidCvv = this.inputValidation('cvv', (str) => str.length ==3 && this.check_chars(str, (chr) => (chr >= '0' && chr <= '9')));
-    var isValidSummary = this.inputValidation('summary', (str) => Number(str) >= 1000 && Number(str) <= 75000);
-    var isValidComment = this.inputValidation('comment', (str) => str.length <= 150);
-    var isValidEmail = this.inputValidation('email', (str) => {
+    this.inputValidation('cardNumber', (str) => (str.length == 16) && this.check_chars(str, (chr) => (chr >= '0' && chr <= '9')));
+    this.inputValidation('cvv', (str) => str.length ==3 && this.check_chars(str, (chr) => (chr >= '0' && chr <= '9')));
+    this.inputValidation('summary', (str) => Number(str) >= 1000 && Number(str) <= 75000);
+    this.inputValidation('comment', (str) => str.length <= 150);
+    this.inputValidation('email', (str) => {
       var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(str);
     });
-    this.valid = isValidCard == 't' && isValidComment == 't' && isValidCvv == 't' && isValidEmail == 't' && isValidSummary == 't';
-    this.get_confirm_actual();
     return;
-  }
-
-  get_confirm_actual()
-  {
-    if (this.valid)
-      $('#confirm-purchase').addClass('disabled')
-    else if ($('#confirm-purchase').hasClass('disabled'))
-      $('#confirm-purchase').removeClass('disabled')
   }
 
   ngOnInit() {
     this.validate_all();
-    this.get_confirm_actual();
   }
     // $(document).ready(function() {
     //     var owner = $('#owner');
